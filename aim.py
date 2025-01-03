eea84e63c0cd6c4543d7890d4c0550f1dd8c2816
AZysk0 <arko.zystem@gmail.com>
2025-01-03 14:38:51 +0200

    basic mouse events automation

--- aim.py
+++ aim.py
@@ -2,26 +2,29 @@ import numpy as np
 import random
 import pyautogui
 
-from control import PIDController
+from control import PIDController, PIDParams
 from vision import FrameProcessorCV
 
 class AutoAimBot:
     
     def __init__(self):
-        self.yawController = PIDController(np.array([0.1, 0.1, 0.1]))
-        self.pitchController = PIDController(np.array([0.1, 0.1, 0.1]))
+        self.yawController = PIDController()
+        self.pitchController = PIDController()
         self.frameProcessor = FrameProcessorCV()
         self.prevTargetPos = (0, 0)  # pos on image
     
-    def chooseTarget(self, targets):
+    def chooseTarget(self, targetCentroids):
         '''targets - list of centroids of bboxes of separated objects'''
         
+        # sort by distance to the center
         
+        # get random from 3 nearest (if more than 3 targets on the screen)
         
         return
     
     def updateCurrentTarget(self):
         '''get centroid that is nearest to target from previous state'''        
+        
         return
     
     def lockTarget(self):
--- events.py
+++ events.py
@@ -1,6 +1,71 @@
+import ctypes
+from typing import Tuple, Any, Literal
+
+
+class Mouse:
+    
+    # init some constans for mouse events
+    # source https://learn.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-mouse_event
+    MOUSEEVENTF_MOVE = 0x0001
+    MOUSEEVENTF_LEFTDOWN = 0x0002
+    MOUSEEVENTF_LEFTUP = 0x0004
+    MOUSEEVENTF_RIGHTDOWN = 0x0008
+    MOUSEEVENTF_RIGHTUP = 0x0010
+    MOUSEEVENTF_MIDDLEDOWN = 0x0020
+    MOUSEEVENTF_MIDDLEUP = 0x0040
+    
+    def __init__(self):
+        self.pressedButtons = set()
+    
+    def moveBy(self, dx, dy):
+        ctypes.windll.user32.mouse_event(self.MOUSEEVENTF_MOVE, dx, dy, 0, 0)
+    
+    def moveTo(self, x, y):
+        ctypes.windll.user32.SetCursorPos(x, y)
+
+    def click(self, mouseBtn: str | Literal[64]) -> None:
+        self.pressBtn(mouseBtn)
+        self.releaseBtn(mouseBtn)
+
+    def pressBtn(self, mouseBtn: str | Literal[64]) -> None:
+        if mouseBtn == "left":
+            ctypes.windll.user32.mouse_event(self.MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0)
+            self.pressedButtons.add("left")
+        elif mouseBtn == "right":
+            ctypes.windll.user32.mouse_event(self.MOUSEEVENTF_RIGHTDOWN, 0, 0, 0, 0)
+            self.pressedButtons.add("right")
+        elif mouseBtn == "middle":
+            ctypes.windll.user32.mouse_event(self.MOUSEEVENTF_MIDDLEDOWN, 0, 0, 0, 0)
+            self.pressedButtons.add("middle")
+        else:
+            raise ValueError(f"Unsupported button: {mouseBtn}")
+
+    def releaseBtn(self, mouseBtn: str | Literal[64]) -> None:
+        if mouseBtn == "left" and "left" in self.pressedButtons:
+            ctypes.windll.user32.mouse_event(self.MOUSEEVENTF_LEFTUP, 0, 0, 0, 0)
+            self.pressedButtons.remove("left")
+        elif mouseBtn == "right" and "right" in self.pressedButtons:
+            ctypes.windll.user32.mouse_event(self.MOUSEEVENTF_RIGHTUP, 0, 0, 0, 0)
+            self.pressedButtons.remove("right")
+        elif mouseBtn == "middle" and "middle" in self.pressedButtons:
+            ctypes.windll.user32.mouse_event(self.MOUSEEVENTF_MIDDLEUP, 0, 0, 0, 0)
+            self.pressedButtons.remove("middle")
+        else:
+            raise ValueError(f"Unsupported or unpressed button: {mouseBtn}")
+
 
 
 class Keyboard:
+    
+    def __init__(self):
+        raise NotImplementedError
+    
+    def pressBtn(self, btn: str) -> None:
+        raise NotImplementedError
+        
+
+
+class KeyboardInputHandler:
     
     def __init__(self):
         raise NotImplementedError
@@ -10,7 +75,5 @@ class Keyboard:
     
     def getState(self):
         raise NotImplementedError
-
-
 
 
