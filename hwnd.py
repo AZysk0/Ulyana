a79aa193049168291b499fb0d4540eea725b2a04
AZysk0 <arko.zystem@gmail.com>
2025-01-05 18:39:51 +0200

    Win32 capture issue, MSS capture implemented

--- hwnd.py
+++ hwnd.py
@@ -10,6 +10,7 @@ import win32gui
 import win32ui
 import win32con
 import win32api
+import win32com.client
 
 from typing import Optional, Dict, Any
 
@@ -19,6 +20,132 @@ from typing import Optional, Dict, Any
 # OpenGL q3-hook 
 # 
 
+
+class WindowCaptureAbstract:
+    
+    def __init__(self, windowTitle: str):
+        self.windowTitle = windowTitle
+        
+        self.hWnd = win32gui.FindWindow(None, windowTitle)
+        if not self.hWnd:
+            raise Exception(f"Window with title '{windowTitle}' not found!")
+
+        window_rect = win32gui.GetWindowRect(self.hWnd)
+        self.offset_x = window_rect[0]
+        self.offset_y = window_rect[1]
+        self.w = window_rect[2] - window_rect[0]
+        self.h = window_rect[3] - window_rect[1]
+        
+        win32gui.SetForegroundWindow(self.hWnd)
+    
+    def listWindowNames(self):
+        def winEnumHandler(hwnd, ctx):
+            if win32gui.IsWindowVisible(hwnd):
+                print(hex(hwnd), win32gui.GetWindowText(hwnd))
+        win32gui.EnumWindows(winEnumHandler, None)
+        
+    def get_screen_position(self, pos):
+        return (pos[0] + self.offset_x, pos[1] + self.offset_y)
+
+
+class WindowCaptureWin32(WindowCaptureAbstract):
+    '''
+    Issue:
+    - Static window frame (quake 3)
+    - To get another frame you need to re-launch window 
+    and after this the same problem with static frame
+    - Windows PowerShell - black image (with debug info)
+    '''
+
+    def __init__(self, windowTitle: str):
+        super().__init__(windowTitle=windowTitle)
+
+    def takeScreenshot(self):
+        
+        # windowDC = windll.user32.GetWindowDC(self.hWnd)
+        
+        win32gui.RedrawWindow(
+            self.hWnd, None, None, 
+            win32con.RDW_INVALIDATE | win32con.RDW_UPDATENOW
+        )
+        
+        # get the window image data
+        windowDC = win32gui.GetWindowDC(self.hWnd)
+        dcObj = win32ui.CreateDCFromHandle(windowDC)
+        memoryDC = dcObj.CreateCompatibleDC()
+        dataBitMap = win32ui.CreateBitmap()
+        dataBitMap.CreateCompatibleBitmap(dcObj, self.w, self.h)
+        memoryDC.SelectObject(dataBitMap)
+        memoryDC.BitBlt((0, 0), (self.w, self.h), dcObj, (0,0), win32con.SRCCOPY)
+        
+        #save the screenshot
+        signedIntsArray = dataBitMap.GetBitmapBits(True)
+        # img = np.fromstring(signedIntsArray, dtype='uint8').reshape(self.h, self.w, 4)[..., :3]
+        img = np.frombuffer(signedIntsArray, dtype='uint8').reshape(self.h, self.w, 4)[..., :3]
+        img = np.ascontiguousarray(img)
+        
+        # Free Resources
+        dcObj.DeleteDC()
+        memoryDC.DeleteDC()
+        win32gui.ReleaseDC(self.hWnd, windowDC)
+        win32gui.DeleteObject(dataBitMap.GetHandle())
+        
+        # print(img.shape)
+        
+        return img
+
+    def _on_resize(self):
+        # update window if resized
+        
+        pass
+
+    def update(self):
+        pass
+
+
+class WindowCaptureMSS(WindowCaptureAbstract):
+    
+    def __init__(self, windowTitle: str):
+        super().__init__(windowTitle=windowTitle)
+    
+    def focusCurrentWindow(self) -> None:
+        '''Error for commented code on Alt+tab'''
+        
+        # if self.hWnd is None:
+        #     raise RuntimeError("Window is not set. Unable to focus.")
+        
+        # foreground_window_title = win32gui.GetWindowText(win32gui.GetForegroundWindow())
+        
+        # if foreground_window_title != self.windowTitle:
+        #     win32gui.SetForegroundWindow(self.hWnd)
+        
+        shell = win32com.client.Dispatch("WScript.Shell")
+        shell.SendKeys('%')
+        win32gui.SetForegroundWindow(self.hWnd)
+    
+    def takeScreenshot(self) -> np.ndarray:
+        # ensure window is always focused
+        
+        # self.focusCurrentWindow()
+        
+        monitor = {
+            "top": self.offset_y,
+            "left": self.offset_x,
+            "width": self.w,
+            "height": self.h
+        }
+        
+        with mss.mss() as sct:
+            screenshot = sct.grab(monitor)
+            
+            img = np.array(screenshot)[:, :, :3]
+            return img
+
+
+
+
+
+# ===================== Some shit i probably will never use ==========
 
 class WindowHandler:
     '''
@@ -44,7 +171,6 @@ class WindowHandler:
         buf = create_unicode_buffer(length + 1)
         windll.user32.GetWindowTextW(hWnd, buf, length + 1)
     
-        # 1-liner alternative: 
         return buf.value if buf.value else None
 
     def focusCurrentWindow(self) -> None:
@@ -64,6 +190,7 @@ class WindowHandler:
             self.hwnd.width,
             self.hwnd.height
         )
+        # slow: 25 fps at most
         screenshot = np.array(pyautogui.screenshot(region=region))
         # return cv.cvtColor(screenshot, cv.COLOR_BGR2RGB)
         return screenshot
@@ -98,102 +225,6 @@ class WindowHandler:
 
     def debugScreenshot(self, save_path: str = "screenshot.png") -> None:
         raise NotImplementedError
-
-
-
-class WindowCapture:
-
-    # properties
-    w = 0
-    h = 0
-    hwnd = None
-    cropped_x = 0
-    cropped_y = 0
-    offset_x = 0
-    offset_y = 0
-
-    # constructor
-    def __init__(self, window_name):
-        # find the handle for the window we want to capture
-        self.hwnd = win32gui.FindWindow(None, window_name)
-        if not self.hwnd:
-            raise Exception('Window not found: {}'.format(window_name))
-
-        # get the window size
-        window_rect = win32gui.GetWindowRect(self.hwnd)
-        self.w = window_rect[2] - window_rect[0]
-        self.h = window_rect[3] - window_rect[1]
-
-        # account for the window border and titlebar and cut them off
-        border_pixels = 8
-        titlebar_pixels = 30
-        self.w = self.w - (border_pixels * 2)
-        self.h = self.h - titlebar_pixels - border_pixels
-        self.cropped_x = border_pixels
-        self.cropped_y = titlebar_pixels
-
-        # set the cropped coordinates offset so we can translate screenshot
-        # images into actual screen positions
-        self.offset_x = window_rect[0] + self.cropped_x
-        self.offset_y = window_rect[1] + self.cropped_y
-
-    def get_screenshot(self):
-        
-        win32gui.InvalidateRect(self.hwnd, None, True)
-        win32gui.UpdateWindow(self.hwnd)
-
-
-        # get the window image data
-        wDC = win32gui.GetWindowDC(self.hwnd)
-        dcObj = win32ui.CreateDCFromHandle(wDC)
-        saveDC = dcObj.CreateCompatibleDC()
-        saveBitMap = win32ui.CreateBitmap()
-        saveBitMap.CreateCompatibleBitmap(dcObj, self.w, self.h)
-        
-        saveDC.SelectObject(saveBitMap)
-        
-        result = windll.user32.PrintWindow(self.hwnd, saveDC.GetSafeHdc(), 0)
-        
-        # saveDC.BitBlt((0, 0), (self.w, self.h), dcObj, (self.cropped_x, self.cropped_y), win32con.SRCCOPY)
-
-        bmpinfo = saveBitMap.GetInfo()
-        bmpstr = saveBitMap.GetBitmapBits(True)
-        
-        # convert the raw data into a format opencv can read
-        
-        img = Image.frombuffer(
-            'RGB',
-            (bmpinfo['bmWidth'], bmpinfo['bmHeight']),
-            bmpstr, 'raw', 'BGRX', 0, 1)
-        
-        imgArray = np.array(img)
-
-        # free resources
-        dcObj.DeleteDC()
-        saveDC.DeleteDC()
-        win32gui.ReleaseDC(self.hwnd, wDC)
-        win32gui.DeleteObject(saveBitMap.GetHandle())
-
-        # some shit for debugging
-        print(result, imgArray.shape, sep='\n')
-
-        return cv.cvtColor(imgArray, cv.COLOR_BGR2RGB)
-        # return imgArray
-
-    # find the name of the window you're interested in.
-    def list_window_names(self):
-        def winEnumHandler(hwnd, ctx):
-            if win32gui.IsWindowVisible(hwnd):
-                print(hex(hwnd), win32gui.GetWindowText(hwnd))
-        win32gui.EnumWindows(winEnumHandler, None)
-
-    # translate a pixel position on a screenshot image to a pixel position on the screen.
-    # pos = (x, y)
-    # WARNING: if you move the window being captured after execution is started, this will
-    # return incorrect coordinates, because the window position is only calculated in
-    # the __init__ constructor.
-    def get_screen_position(self, pos):
-        return (pos[0] + self.offset_x, pos[1] + self.offset_y)
 
 
 
